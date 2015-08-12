ebf2f1e0d54a8f23cf422047d29342773e8ff74c
Ashutosh Singh <dh_ashu@outlook.com>
2015-08-12 07:35:02 +0530

    Adding Solution for PS3_4

--- DataAnalysisWithR/PS3/_3.R
+++ DataAnalysisWithR/PS3/_3.R
@@ -8,7 +8,7 @@ summary(diamonds$price)
 
 #plot price data 
 qplot(price, data=diamonds)
-
+mean(diamonds$price)
 
 ##############################
 ######Answer to P3
@@ -16,3 +16,16 @@ qplot(price, data=diamonds)
 # The number of diamonds are  decreasing with increasing in price. 
 # The mean diamond price is 3933$ and the median price is 2401$  
 ##############################
+
+##############################
+######Answer to Q4
+#Diamonds less than $500
+nrow(subset(diamonds,price<500))
+
+#Diamonds less than $250
+nrow(subset(diamonds,price<250))
+
+#Diamonds less than $15000
+nrow(subset(diamonds,price>=15000))
+##############################
+
